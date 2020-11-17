da8c3cdbc9f1fd021daf11d257cd13ed1a1a5b9b
Marvin W <marvwood12@gmail.com>
2020-11-17 08:35:16 -0500

    removed uneeded annotations

--- src/main/java/com/tts/ecommerce/models/User.java
+++ src/main/java/com/tts/ecommerce/models/User.java
@@ -2,11 +2,10 @@ package com.tts.ecommerce.models;
 
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.util.Collection;
 import java.util.Map;
@@ -15,7 +14,7 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.GrantedAuthority;
 
-
+@Entity
 public class User implements UserDetails {
 	
 	private static final long serialVersionUID = 1L;
@@ -23,10 +22,6 @@ public class User implements UserDetails {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="user_id")
     private Long id;
-
-	@Email(message = "Please provide a valid email")
-	@NotEmpty(message = "Please provide an email")
-	private String email;
 
 	@Length(min = 3, message = "Your username must have at least 3 characters")
 	@Length(max = 15, message = "Your username cannot have more than 15 characters")
@@ -43,18 +38,6 @@ public class User implements UserDetails {
 	public User() {
 	}
 
-	public User(Long id,
-			@Email(message = "Please provide a valid email") @NotEmpty(message = "Please provide an email") String email,
-			@Length(min = 3, message = "Your username must have at least 3 characters") @Length(max = 15, message = "Your username cannot have more than 15 characters") @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces") String username,
-			@Length(min = 5, message = "Your password must have at least 5 characters") String password,
-			Map<Product, Integer> cart) {
-		this.id = id;
-		this.email = email;
-		this.username = username;
-		this.password = password;
-		this.cart = cart;
-	}
-
 	public Long getId() {
 		return id;
 	}
@@ -63,13 +46,6 @@ public class User implements UserDetails {
 		this.id = id;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public String getUsername() {
 		return username;
