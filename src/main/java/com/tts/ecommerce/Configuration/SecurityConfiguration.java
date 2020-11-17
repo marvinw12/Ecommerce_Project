d0fffa1ccc4bddb635d56776e899629579183749
Marvin W <marvwood12@gmail.com>
2020-11-17 08:30:34 -0500

    added config and websecurity annotations

--- src/main/java/com/tts/ecommerce/Configuration/SecurityConfiguration.java
+++ src/main/java/com/tts/ecommerce/Configuration/SecurityConfiguration.java
@@ -3,12 +3,16 @@ package com.tts.ecommerce.Configuration;
 import com.tts.ecommerce.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     	
