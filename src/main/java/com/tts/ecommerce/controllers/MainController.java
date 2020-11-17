f79ac333fdb7bdc77c483a72a7904ce1907a6c65
Marvin W <marvwood12@gmail.com>
2020-11-17 08:32:31 -0500

    added products to main controller

--- src/main/java/com/tts/ecommerce/controllers/MainController.java
+++ src/main/java/com/tts/ecommerce/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.tts.ecommerce.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.tts.ecommerce.models.Product;
@@ -19,9 +20,43 @@ import org.springframework.web.bind.annotation.RequestParam;
    public class MainController {
        @Autowired
        ProductService productService;
+
+       public void addNew() {
+        List<Product> allProducts = productService.findAll();
+
+        if (allProducts.isEmpty()) {
+
+            List<Product> newProducts = new ArrayList<Product>();
+
+            newProducts.add(new Product(4, (float) 1500.00, "Nike Zoom Kobe 6 Grinch", "Kobe 6", "Nike", "basketball",
+                    "images/KobeGrinch.jpeg"));
+
+            newProducts.add(new Product(3, (float) 1000.00, "Air Jordan 1 Retro Mocha", "Air Jordan 1", "Jordan", "basketball",
+                    "images/AJ1Mocha.jpeg"));
+
+            newProducts.add(new Product(12, (float) 800.00, "Nike SB Mens Dunk Low 'Ben & Jerry's'", "Nike SB Mens Dunk Low", "Nike", "skateboarding",
+                    "images/NikeBenJerry.jpeg"));
+
+            newProducts.add(new Product(7, (float) 700.00, "Adidas Yeezy Boost 700 Mauve", "Adidas Yeezy Boost 700", "Adidas", "running",
+                    "images/YZY700.jpeg"));
+            
+            newProducts.add(new Product(7, (float) 700.00, "Adidas Pharrell X NMD Human Race 'Yellow'", "Adidas NMD", "Adidas", "running",
+                    "images/AdidasHMR.jpeg"));
+
+            for (Product product : newProducts) {
+                productService.save(product);
+            }
+        } else {
+
+            System.out.println("You don't need more items!");
+        }
+    }
+    
  
        @GetMapping("/")
        public String main() {
+
+           addNew(); 
            return "main";
        }
  
@@ -53,6 +88,7 @@ import org.springframework.web.bind.annotation.RequestParam;
        public String about() {
            return "about";
        }
-   }
+    }
+   
 
 
