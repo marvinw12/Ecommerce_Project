6bdaf504d871824e931b9e415970d164e160d5b5
Marvin W <marvwood12@gmail.com>
2020-11-21 21:40:01 -0500

    model

--- src/main/java/com/tts/ecommerce/models/Product.java
+++ src/main/java/com/tts/ecommerce/models/Product.java
@@ -19,7 +19,7 @@ public class Product {
 
     private String brand;
 
-    private String price;
+    private Float price;
 
     private String description;
 
@@ -29,77 +29,71 @@ public class Product {
 	public Product() {
 	}
 
-	public Product(Long id, String name, int quantity, String category, String brand, String price, String description,
-			String image) {
-		this.id = id;
-		this.name = name;
-		this.quantity = quantity;
-		this.category = category;
-		this.brand = brand;
-		this.price = price;
-		this.description = description;
-		this.image = image;
-	}
-
-
-    public Product(int i, float f, String string, String string2, String string3, String string4, String string5) {
-	}
-
+    public Product(int quantity, Float price, String name, String description, String brand, String category,
+    String image) {
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+        this.brand = brand;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+        }
 
 	public Long getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getQuantity() {
+	public int getQuantity() {
 		return quantity;
 	}
 
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getImage() {
@@ -110,12 +104,21 @@ public class Product {
 		this.image = image;
 	}
 
+	@Override
+	public String toString() {
+		return "Product [brand=" + brand + ", category=" + category + ", description=" + description + ", id=" + id
+				+ ", image=" + image + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+    
 
-    @Override
-    public String toString() {
-        return "Product [brand=" + brand + ", category=" + category + ", description=" + description + ", id=" + id
-                + ", name=" + name + ", price=" + price + "]";
-    }
+        
+
+
+
+
+
+
+
 
 
 
